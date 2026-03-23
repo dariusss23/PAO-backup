788afdb6fcbb60c724b2965b1e5f96e0223fb152
dariusss23 <savadarius23@gmail.com>
2026-03-23 13:16:56 +0200

    Add files via upload

---
+++ laboratory05/angajati/Main.java
@@ -0,0 +1,63 @@
+package com.pao.laboratory05.angajati;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        AngajatService service = AngajatService.getInstance();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n===== Gestionare Angajați =====");
+            System.out.println("1. Adaugă angajat");
+            System.out.println("2. Listare după salariu (descrescător)");
+            System.out.println("3. Caută după departament");
+            System.out.println("4. Afișează toți angajații (nesortat)");
+            System.out.println("0. Ieșire");
+            System.out.print("Opțiune: ");
+
+            int optiune = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (optiune) {
+                case 1:
+                    System.out.print("Nume angajat: ");
+                    String nume = scanner.nextLine();
+                    System.out.print("Nume departament: ");
+                    String numeDept = scanner.nextLine();
+                    System.out.print("Locație departament: ");
+                    String locatieDept = scanner.nextLine();
+                    System.out.print("Salariu: ");
+                    double salariu = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    Departament d = new Departament(numeDept, locatieDept);
+                    service.addAngajat(new Angajat(nume, d, salariu));
+                    break;
+
+                case 2:
+                    System.out.println("--- Angajați sortați după salariu ---");
+                    service.listBySalary();
+                    break;
+
+                case 3:
+                    System.out.print("Introduceți numele departamentului: ");
+                    String cautaDept = scanner.nextLine();
+                    service.findByDepartament(cautaDept);
+                    break;
+
+                case 4:
+                    System.out.println("--- Toți angajații ---");
+                    service.printAll();
+                    break;
+
+                case 0:
+                    System.out.println("Ieșire din program...");
+                    return;
+
+                default:
+                    System.out.println("Opțiune invalidă!");
+            }
+        }
+    }
+}
---
+++ laboratory05/playlist/Main.java
@@ -0,0 +1,21 @@
+package com.pao.laboratory05.playlist;
+
+public class Main {
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist("Road Trip");
+
+        playlist.addSong(new Song("Waterloo", "ABBA", 174));
+        playlist.addSong(new Song("Bohemian Rhapsody", "Queen", 354));
+        playlist.addSong(new Song("Imagine", "John Lennon", 187));
+        playlist.addSong(new Song("Smells Like Teen Spirit", "Nirvana", 301));
+
+        System.out.println("=== " + playlist.getName() + " ===");
+        System.out.println("Durata totală: " + playlist.getTotalDuration() + "s\n");
+
+        System.out.println("--- Sortate după titlu ---");
+        playlist.printSortedByTitle();
+
+        System.out.println("\n--- Sortate după durată ---");
+        playlist.printSortedByDuration();
+    }
+}
